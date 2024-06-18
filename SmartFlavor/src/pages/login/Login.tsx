89759a519ecb042e134e5b71d7823904bbe2c421
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-18 19:18:53 -0400

    update for testing

--- SmartFlavor/src/pages/login/Login.tsx
+++ SmartFlavor/src/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonFooter, IonPage } from "@ionic/react";
+import { IonContent, IonFooter, IonImg, IonPage } from "@ionic/react";
 import "./Login.css"
 import "../../components/header-interact/Header"
 import Header from "../../components/header-interact/Header";
@@ -6,13 +6,16 @@ import Footer from "../../components/footers/footer-3/Footer";
 import SearchBar from "../../components/search-bar/SearchBar";
 import CommentContainer from "../../components/comment/comment-container/CommentContainer";
 import ProfileContainer from "../../components/profile-container/ProfileContainer";
+import WelcomeMessage from "../../components/welcome-message/WelcomeMessage";
+import ChatContainer from "../../components/chat-container/ChatContainer";
+import Presentation from "../../components/presentation-search/Presentation";
 
 const Login: React.FC = () => {
     return (
         <IonPage>
               <IonContent>
                   <Header/>
-                  <ProfileContainer user_name={"Chef Ruperto"} post_num={23} follow_num={600} following_num={450} profile_photo={"images/profile_photos/chef 1.jpg"} description={"Como estás?"}></ProfileContainer>
+                  <IonImg src="videos/Download.mp4"></IonImg>
               </IonContent>
         </IonPage>
     );
