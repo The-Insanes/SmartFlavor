6744b6e986b74e2851908703ab53f9b02002399e
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-17 22:11:46 -0400

    add new component ProfileContainer

---
+++ SmartFlavor/src/components/profile-container/ProfileContainer.tsx
@@ -0,0 +1,63 @@
+import { IonAvatar, IonIcon, IonLabel } from "@ionic/react";
+import { colorPalette, pencil } from "ionicons/icons";
+import "./ProfileContainer.css"
+
+interface ContainerProps {
+    className?: string | undefined
+    user_name: string,
+    post_num: number,
+    follow_num: number,
+    following_num: number,
+    profile_photo: string,
+    description: string
+};
+
+const ProfileContainer: React.FC<ContainerProps> = (props) => {
+    return (
+        <div className={props.className}>
+            <div className="profile-background">
+                <div className="profile-container">
+                    <div className="top-profile-container">
+                        <div className="public-info">
+                            <div className="profile-image">
+                                <IonAvatar>
+                                    <img src={props.profile_photo}></img>
+                                </IonAvatar>
+
+                                <IonLabel>{props.user_name}</IonLabel>
+                            </div>
+
+                            <div className="profile-left-container">
+                                <div className="profile-option">
+                                    <IonIcon icon={colorPalette}></IonIcon>
+                                    <IonIcon icon={pencil}></IonIcon>
+                                </div>
+
+                                <div className="indicator-container">
+                                    <div className="indicator">
+                                        <IonLabel>{props.post_num}</IonLabel>
+                                        <IonLabel>Publicaciones</IonLabel>
+                                    </div>
+
+                                    <div className="indicator">
+                                        <IonLabel>{props.follow_num}</IonLabel>
+                                        <IonLabel>Seguidores</IonLabel>
+                                    </div>
+
+                                    <div className="indicator">
+                                        <IonLabel>{props.following_num}</IonLabel>
+                                        <IonLabel>Seguidos</IonLabel>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <IonLabel>{props.description}</IonLabel>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileContainer;
