3246db99b15cd1fa88343f39bc4ee69315eba825
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-23 01:15:08 -0400

    add new functions for example close window when click outside of the container

--- SmartFlavor/src/components/comment/comment-section/CommentSection.tsx
+++ SmartFlavor/src/components/comment/comment-section/CommentSection.tsx
@@ -1,7 +1,7 @@
 import "./CommentSection.css"
 import { IonAvatar, IonContent, IonInfiniteScroll, IonInfiniteScrollContent, IonInput, IonList } from "@ionic/react";
 import CommentContainer from "../comment-container/CommentContainer";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Comment {
     profile_photo: string,
@@ -14,7 +14,9 @@ interface Comment {
 interface ContainerProps {
     className?: string | undefined
     comments?: Comment[] | undefined,
-    profile_photo: string
+    profile_photo: string,
+    close: () => void,
+    view: boolean
 }
 
 const PAGE_SIZE = 10;
@@ -22,12 +24,13 @@ const PAGE_SIZE = 10;
 const CommentSection: React.FC<ContainerProps> = (props) => {
     const [comments, setComments] = useState<Comment[]>([])
     const [currentPage, setCurrentPage] = useState<number>(0);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const loadMoreComments = () => {
         if(!props.comments) return;
-
+        
         const nextPage = currentPage + 1;
-        const startIndex = currentPage + PAGE_SIZE;
+        const startIndex = currentPage * PAGE_SIZE;
         const endIndex = nextPage * PAGE_SIZE;
         const newComments = props.comments.slice(startIndex, endIndex);
 
@@ -41,23 +44,46 @@ const CommentSection: React.FC<ContainerProps> = (props) => {
     }
 
     useEffect(() => {
-        console.log(props.comments);
-        setComments([]);
-        setCurrentPage(0);
-        loadMoreComments();
+        const handleClickOutside = (event: MouseEvent) => {
+            if (containerRef.current && !containerRef.current.contains(event.target as Node) && props.view) {
+                props.close();
+            }
+        };
 
-    }, [props.comments])
+        const handleBackButton = (e: any) => {
+            if (props.view) {
+                e.preventDefault();
+                e.stopPropagation();
+                props.close();
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener('backbutton', handleBackButton);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener('backbutton', handleBackButton);
+        };
+    }, [props.close, props.view]);
+
+    useEffect(() => {
+        setCurrentPage(-1);
+        setComments([]);
+        loadMoreComments();
+        
+    }, [])
 
 
     return (
-        <div className={props.className}>
+        <div ref={containerRef} className={props.className}>
             <div className="comment-section-container">
                 <div className="rectangle-up-down"></div>
                 
                 <IonContent className="comment-list">
                     <IonList className="comment-items">
                         {comments.map((commment, index) => (
-                            <CommentContainer className="user-comment" user_name={commment.user_name} num_likes={commment.likes} description={commment.description} profile_photo={commment.profile_photo} time={commment.time}/>
+                            <CommentContainer className="user-comment" user_name={commment.user_name} num_likes={commment.likes} description={commment.description} profile_photo={commment.profile_photo} time={commment.time} key={index}/>
                         ))}
                     </IonList>
 
@@ -66,7 +92,7 @@ const CommentSection: React.FC<ContainerProps> = (props) => {
                     </IonInfiniteScroll>
                 </IonContent>
 
-                <IonInput className="comment-input" placeholder="Agrega un comentario">
+                <IonInput className="comment-input" placeholder="Agrega un comentario" clearInput>
                     <IonAvatar className="comment-profile-avatar" slot="start">
                         <img src={props.profile_photo}></img>
                     </IonAvatar>
