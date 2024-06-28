bfd260e5a0da03598126caa2a4b422e3707d9366
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-27 20:38:44 -0400

    add all routes for the page

--- SmartFlavor/src/App.tsx
+++ SmartFlavor/src/App.tsx
@@ -4,8 +4,8 @@ import { IonReactRouter } from '@ionic/react-router';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Search from './pages/search/Search';
-import EditProfile from './pages/editprofile/Editprofile';
-import ReportPost from './pages/reportpost/ReportPost';
+import EditProfile from './pages/edit-profile/Editprofile';
+import ReportPost from './pages/report/post/ReportPost';
 import ReportUsers from './pages/report/users/ReportUsers';
 
 /* Core CSS required for Ionic components to work properly */
@@ -26,6 +26,13 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import EditPost from './pages/edit-post/EditPost';
+import Main from './pages/main/user/Main';
+import Camera from './pages/camera/Camera';
+import RecommendRecipe from './pages/recommend-recipe/RecommendRecipe';
+import Profile from './pages/profile/Profile';
+import AdminMain from './pages/main/admin/AdminMain';
+import LoadPage from './pages/load-page/LoadPage';
 
 setupIonicReact();
 
@@ -33,11 +40,64 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <ReportUsers />
+        <Route exact path="/login">
+          <Login/>
         </Route>
+
         <Route exact path="/">
-          <Redirect to="/login" />
+          <Redirect to="/login"/>
+        </Route>
+
+        <Route exact path="/EditPost">
+          <EditPost/>
+        </Route>
+
+        <Route exact path="/camera">
+          <Camera/>
+        </Route>
+
+        <Route exact path="/register">
+          <Register/>
+        </Route>
+
+        <Route exact path="/search_video">
+          <Search/>
+        </Route>
+
+        <Route exact path="/report_user">
+          <ReportUsers/>
+        </Route>
+
+        <Route exact path="/report_post">
+          <ReportPost/>
+        </Route>
+
+        <Route exact path="/recomend_recipe">
+          <RecommendRecipe/>
+        </Route>
+        
+        <Route exact path="/user_profile">
+          <Profile/>
+        </Route>
+
+        <Route exact path="/user-home">
+          <Main/>
+        </Route>
+
+        <Route exact path="/admin_home">
+          <AdminMain/>
+        </Route>
+
+        <Route exact path="/load_page">
+          <LoadPage/>
+        </Route>
+
+        <Route exact path="/edit_profile">
+          <EditProfile/>
+        </Route>
+
+        <Route exact path="/edit_post">
+          <EditPost/>
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
